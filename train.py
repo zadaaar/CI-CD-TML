bee5c8e22c19e9108df28bfd1902126ad8bd23fb
zadaaar <zadaalfarras@gmail.com>
2025-04-08 23:11:12 +0000

    Update with new results

--- train.py
+++ train.py
@@ -22,9 +22,9 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-## Pipeline 
-cat_col = [1,2,3]
-num_col = [0,4]
+## Pipeline
+cat_col = [1, 2, 3]
+num_col = [0, 4]
 
 transform = ColumnTransformer(
     [
